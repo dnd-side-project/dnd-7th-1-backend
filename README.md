3feb4bd46d4acafbe739dbea0701d8b1042026ce
ChanoPark <koc081900@korea.ac.kr>
2022-07-15 15:11:41 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## DND-7th-1-backend
+
+### Technology Stack 
+- Spring boot 2.7.1
+- Gradle 7.4.1
+- Data JPA
+- MySQL
+- AWS EC2
+- ETC ...
---
+++ src/test/java/com/dnd/ground/GroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd.ground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
