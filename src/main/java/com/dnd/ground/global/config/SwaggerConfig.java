a73572df48a56d47fe88b92f7408878dfa0075f7
ChanoPark <koc081900@korea.ac.kr>
2022-07-18 16:44:46 +0900

    CHORE: Logger, Swagger 추가

---
+++ src/main/java/com/dnd/ground/global/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.dnd.ground.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * Swagger 설정 파일
+ *
+ * @author 박찬호
+ */
+
+@Configuration
+public class SwaggerConfig {
+    private String version = "V0.1";
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("DND 7th team.1")
+                .description("DND 7th team.1 API document")
+                .version(version)
+                .build();
+    }
+}
