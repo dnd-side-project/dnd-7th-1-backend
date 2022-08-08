440661d9349896fd240d81bd59ff51dacf326535
ChanoPark <koc081900@korea.ac.kr>
2022-08-08 18:35:49 +0900

    MODIFY: 메인화면 조회 시 챌린지 관련 로직 변경
    1. 메인 화면 챌린지 관련 로직 변경(showHome())
    2. 관련 조회 쿼리 추가(챌린지에 참여한 회원 정보 관련)
    3. HomeResponseDto userMatrix 필드명 변경(userMatrix -> userMatrices)

--- src/main/java/com/dnd/ground/domain/challenge/repository/UserChallengeRepository.java
+++ src/main/java/com/dnd/ground/domain/challenge/repository/UserChallengeRepository.java
@@ -1,5 +1,6 @@
 package com.dnd.ground.domain.challenge.repository;
 
+import com.dnd.ground.domain.challenge.Challenge;
 import com.dnd.ground.domain.challenge.UserChallenge;
 import com.dnd.ground.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,13 +14,16 @@ import java.util.Optional;
  * @description 회원-챌린지 간 조인엔티티와 관련한 레포지토리
  * @author  박찬호
  * @since   2022-08-03
- * @updated 1. User를 통해 진행 중인 챌린지가 있는 회원 조회
- *          2. User를 통해 진행 중인 챌린지가 없는 회원 조회
- *          - 2022.08.05 박찬호
+ * @updated 1. 챌린지에 참여한 회원 정보 조회
+ *          - 2022.08.08 박찬호
  */
 
 public interface UserChallengeRepository extends JpaRepository<UserChallenge, Long> {
     List<UserChallenge> findByUser(User user);
+
+    //User로 챌린지 목록 조회
+    @Query("select c from Challenge c inner join UserChallenge uc on uc.user=:user")
+    List<Challenge> findChallenge(@Param("user") User user);
 
     //User를 통해 진행 중인 챌린지가 있는 회원 조회
     @Query("select u from User u inner join UserChallenge uc on uc.user=:user where " +
@@ -30,4 +34,9 @@ public interface UserChallengeRepository extends JpaRepository<UserChallenge, Lo
     @Query("select u from User u inner join UserChallenge uc on uc.user=:user where " +
             "(uc.challenge = (select c from Challenge c where c=uc.challenge and c.status<>'Progress') and u = :user) ")
     Optional<User> findNotChallenging(@Param("user") User user);
+
+    //챌린지에 포함된 회원 조회
+    @Query("select uc.user from UserChallenge uc where uc.challenge=:challenge")
+    List<User> findChallengeUsers(@Param("challenge") Challenge challenge);
+
 }
