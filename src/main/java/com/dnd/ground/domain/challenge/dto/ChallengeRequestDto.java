bd51c576220a7031eb1ff79c29085b02510ff7e0
ChanoPark <koc081900@korea.ac.kr>
2022-08-08 20:21:49 +0900

    FEAT: 챌린지 수락/거절 기능 구현 및 주최자 관련 부분 추가
    1. 챌린지 수락/거절 기능 구현
    2. 챌린지 상태(ChallengeStatus) 추가(Master, Reject)
    3. 챌린지 생성 시, 주최자는 Master 상태로 변경

---
+++ src/main/java/com/dnd/ground/domain/challenge/dto/ChallengeRequestDto.java
@@ -0,0 +1,31 @@
+package com.dnd.ground.domain.challenge.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * @description 챌린지와 관련한 Request DTO
+ * @author  박찬호
+ * @since   2022-08-08
+ * @updated 1. 유저-챌린지 정보를 얻기 위한 이너 클래스(Info) 생성
+ *          - 2022.08.08 박찬호
+ */
+
+
+@Data
+public class ChallengeRequestDto {
+
+    //유저-챌린지 정보를 위한 이너 클래스
+    @Data
+    static public class Info {
+        @NotNull(message = "UUID가 필요합니다.")
+        @ApiModelProperty(value="UUID", example="32개의 문자", required = true)
+        private String uuid;
+
+        @NotNull(message = "닉네임이 필요합니다.")
+        @ApiModelProperty(value="회원 닉네임", example="Nickname", required = true)
+        private String nickname;
+    }
+}
