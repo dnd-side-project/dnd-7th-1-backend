353ecb076def7ab5918fc0ef7e93d6f33c58dd5b
ChanoPark <koc081900@korea.ac.kr>
2022-08-24 23:15:10 +0900

    FEAT: 전역 예외 처리 구현

---
+++ src/main/java/com/dnd/ground/global/exception/ErrorResponse.java
@@ -0,0 +1,46 @@
+package com.dnd.ground.global.exception;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.FieldError;
+
+import java.util.List;
+
+/**
+ * @description 예외 응답 메시지 포맷 클래스
+ * @author  박찬호
+ * @since   2022-08-24
+ * @updated 1. 클래스 생성
+ *          - 2022.08.24 박찬호
+ */
+
+@Getter
+@Builder
+@RequiredArgsConstructor
+public class ErrorResponse {
+
+    private final String code;
+    private final String message;
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY) //값이 없으면 JSON 포함X
+    private final List<ValidationError> errors;
+
+    /*@Valid를 활용해 유효성 검증 시 발생하는 예외처리*/
+    @Getter
+    @Builder
+    @RequiredArgsConstructor
+    public static class ValidationError {
+
+        private final String field;
+        private final String message;
+
+        public static ValidationError of(final FieldError fieldError) {
+            return ValidationError.builder()
+                    .field(fieldError.getField())
+                    .message(fieldError.getDefaultMessage())
+                    .build();
+        }
+    }
+}
