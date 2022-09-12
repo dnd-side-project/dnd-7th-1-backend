43d92887367bdbe266e673b11ad098354dc84097
seheon <quwe153@naver.com>
2022-09-12 19:06:22 +0900

    [FEAT] 톰캣 타임존 설정

--- src/main/java/com/dnd/ground/global/config/TomcatConfig.java
+++ src/main/java/com/dnd/ground/global/config/TomcatConfig.java
@@ -5,11 +5,17 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.TimeZone;
+
 /**
  * @description 스프링부트 톰캣 설정 파일
  * @author  박세헌, 박찬호
  * @since   2022-09-12
- * @updated 1. 생성 - 2022-09-12
+ * @updated 1. 특수문자 처리
+ *          2. 타임존 설정(Asia/Seoul)
+ *          - 2022-09-12 박세헌
  */
 
 @Configuration
@@ -20,5 +26,10 @@ public class TomcatConfig
     public void customize(TomcatServletWebServerFactory factory) {
         factory.addConnectorCustomizers((TomcatConnectorCustomizer)
                 connector -> connector.setProperty("relaxedQueryChars", "<>[\\]^`{|}"));
+    }
+
+    @PostConstruct
+    public void start() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 }
