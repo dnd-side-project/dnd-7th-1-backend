583bbec7cd38c4633745a12fd08469fa13962d56
ChanoPark <koc081900@korea.ac.kr>
2022-10-29 21:22:33 +0900

    FEAT: 카카오 친구 목록 조회 API 구현
    1.현재 15씩 페이징 해놓은 상태.

--- src/main/java/com/dnd/ground/domain/user/dto/KakaoDto.java
+++ src/main/java/com/dnd/ground/domain/user/dto/KakaoDto.java
@@ -1,17 +1,19 @@
 package com.dnd.ground.domain.user.dto;
 
+import com.dnd.ground.domain.friend.FriendStatus;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.Map;
+import java.util.List;
 
 /**
  * @description 카카오 API를 사용할 때 필요한 DTO
  * @author  박찬호
  * @since   2022-08-23
- * @updated 1. 카카오 회원 정보 조회 API 수정
- *          - 2022.09.09 박찬호
+ * @updated 1.친구 목록 조회 관련 DTO 생성
+ *          - 2022.10.29 박찬호
  */
 
 @Data
@@ -54,5 +56,57 @@ public class KakaoDto {
 
         @ApiModelProperty(value="프로필 사진 URI(카카오 프로필 사용 시 kakao/카카오회원번호)", example="http:\\/\\/k.kakaocdn.net\\/dn\\/uQVeo\\/btrLgESJyjg\\/Pff3k36lRWkQ98ebAlexv1\\/img_640x640.jpg")
         private String picturePath;
+    }
+
+    //카카오 친구 목록 조회 결과
+    @Data
+    public static class FriendsInfoFromKakao {
+        private List<KakaoFriend> elements;
+        private Integer total_count;
+        private String before_url;
+        private String after_url;
+        private Integer favorite_count;
+
+        //각 카카오 친구의 정보
+        @Data
+        public static class KakaoFriend {
+            private Long id;
+            private String uuid;
+            private Boolean favorite;
+            private String profile_nickname;
+            private String profile_thumbnail_image;
+        }
+    }
+
+    @Data
+    @NoArgsConstructor
+    public static class kakaoFriendResponse {
+        @ApiModelProperty(value="다음 친구 목록 요청시 필요한 offset", example="16")
+        private Integer nextOffset;
+
+        @ApiModelProperty(value="이번 페이지의 친구 개수(몇명인지)", example="16")
+        private Integer size;
+
+        @ApiModelProperty(value="마지막 페이지인지(true이면 다음 offset은 0)", example="true")
+        private Boolean isLast;
+
+        @ApiModelProperty(value="친구 정보 리스트", example="[{'nickname':'NickA', 'kakaoName':'박찬호','status':'accept','picturePath':'http:\\/\\/k.kakaocdn.net\\/dn\\/uQVeo\\/btrLgESJyjg\\/Pff3k36lRWkQ98ebAlexv1\\/img_640x640.jpg'}]")
+        private List<FriendsInfo> friendsInfo;
+
+        @Data
+        @Builder
+        public static class FriendsInfo {
+            @ApiModelProperty(value="네모두에서의 닉네임", example="NickA")
+            private String nickname;
+
+            @ApiModelProperty(value="카카오톡에서의 이름", example="박찬호")
+            private String kakaoName;
+
+            @ApiModelProperty(value="나와 카카오친구와의 관계(Accept:친구, Requesting:요청중, ResponseWait:응답대기중, NoFriend:친구아님)", example="Accept")
+            private FriendStatus status;
+
+            @ApiModelProperty(value="프로필 사진 URI(카카오 프로필 사용 시 kakao/카카오회원번호)", example="http:\\/\\/k.kakaocdn.net\\/dn\\/uQVeo\\/btrLgESJyjg\\/Pff3k36lRWkQ98ebAlexv1\\/img_640x640.jpg")
+            private String picturePath;
+        }
     }
 }
