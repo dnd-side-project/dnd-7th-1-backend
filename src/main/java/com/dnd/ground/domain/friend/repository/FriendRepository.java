e39f2faba7429088b14dcb0f74ae8678cc4e17dd
ChanoPark <koc081900@korea.ac.kr>
2023-03-06 17:11:09 +0900

    MODIFY: 친구 저장 방식 변경에 따른 수정
    1.애플리케이션 단에서 복잡도가 불필요하게 올라감에 따라, 친구를 수락하는 경우 양방향으로 저장.
    2.이에 따라 관련된 메소드를 전체 수정

--- src/main/java/com/dnd/ground/domain/friend/repository/FriendRepository.java
+++ src/main/java/com/dnd/ground/domain/friend/repository/FriendRepository.java
@@ -15,16 +15,12 @@ import java.util.Optional;
  * @description 친구와 관련한 레포지토리
  * @author  박찬호
  * @since   2022-08-01
- * @updated 1.요청받은 친구 목록 조회 쿼리 추가
- *          - 2022.10.29 박찬호
+ * @updated 1.친구 저장 방식 변경에 따라 상태 조회 쿼리 반환 타입 변경
+ *          2.미사용 쿼리 제거(user로 친구 조회)
+ *          - 2023.03.06 박찬호
  */
 
 public interface FriendRepository extends JpaRepository<Friend, Long>, FriendQueryRepository {
-
-    //User를 통해 친구 목록 조회
-    @Query("select f from Friend f where f.user = :user and f.status='ACCEPT'")
-    List<Friend> findFriendsByUser(@Param("user") User user);
-
     //요청 받은 친구 목록 조회
     @Query("SELECT f.user from Friend f where f.friend=:user and f.status='WAIT'")
     Slice<User> findReceiveRequest(@Param("user") User user, PageRequest pageRequest);
@@ -32,11 +28,11 @@ public interface FriendRepository extends JpaRepository<Friend, Long>, FriendQue
     @Query("select f from Friend f where f.user=:user and f.status='ACCEPT'")
     Slice<Friend> findFriendsByUserWithPaging(@Param("user") User user, PageRequest pageRequest);
 
-    Optional<Friend> findById(Long id);
+    Optional<Friend> findById(@Param("id") Long id);
 
     //친구 관계 여부 조회
     @Query("select f from Friend f where (f.user=:user and f.friend=:friend) or (f.user=:friend and f.friend=:user)")
-    Optional<Friend> findFriendRelation(@Param("user") User user, @Param("friend") User friend);
+    List<Friend> findFriendRelation(@Param("user") User user, @Param("friend") User friend);
 
     //수락 여부 상관 없이 Friend 테이블에 있는 데이터 조회
     @Query("select f from Friend f where (f.friend =:user or f.user = :user)")
