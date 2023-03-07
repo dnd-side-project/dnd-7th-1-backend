67090826211f8908969d891f4c8a0146e15bd90d
ChanoPark <koc081900@korea.ac.kr>
2023-03-07 20:48:34 +0900

    MODIFY: Matrix 엔티티 필드 변경
    1.위도, 경도가 따로 관리되는 것을 Point 타입으로 통일
    2.형변환을 위한 Util 클래스 생성 (+SpanDelta 계산)
    3.영역 조회를 위한 조건 클래스의 생성자 추가(MatrixCond)

--- src/main/java/com/dnd/ground/domain/matrix/dto/MatrixCond.java
+++ src/main/java/com/dnd/ground/domain/matrix/dto/MatrixCond.java
@@ -3,8 +3,10 @@ package com.dnd.ground.domain.matrix.dto;
 import com.dnd.ground.domain.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 /**
  * @description QueryDSL으로 영역 조회를 하기 위한 조건 클래스
@@ -13,9 +15,8 @@ import java.time.LocalDateTime;
  * @updated 1.생성자 추가
  *          - 2023-03-05 박찬호
  */
-
-
 @Getter
+@Setter
 @AllArgsConstructor
 public class MatrixCond {
     private User user;
@@ -23,6 +24,7 @@ public class MatrixCond {
     private Double spanDelta;
     private LocalDateTime started;
     private LocalDateTime ended;
+    private Set<User> users;
 
     public MatrixCond(User user, LocalDateTime started, LocalDateTime ended) {
         this.user = user;
@@ -32,5 +34,36 @@ public class MatrixCond {
 
     public MatrixCond(User user) {
         this.user = user;
+    }
+
+    public MatrixCond(Set<User> users, Location location, Double spanDelta) {
+        this.users = users;
+        this.location = location;
+        this.spanDelta = spanDelta;
+    }
+
+    public MatrixCond(User user, Location location, Double spanDelta, LocalDateTime started, LocalDateTime ended) {
+        this.user = user;
+        this.location = location;
+        this.spanDelta = spanDelta;
+        this.started = started;
+        this.ended = ended;
+    }
+
+    public MatrixCond(Set<User> users, Location location, Double spanDelta, LocalDateTime started, LocalDateTime ended) {
+        this.users = users;
+        this.location = location;
+        this.spanDelta = spanDelta;
+        this.started = started;
+        this.ended = ended;
+    }
+
+    public MatrixCond(MatrixRequestDto request, User user, Set<User> users) {
+        this.user = user;
+        this.users = users;
+        this.location = request.getLocation();
+        this.spanDelta = request.getSpanDelta();
+        this.started = request.getStarted();
+        this.ended = request.getEnded();
     }
 }
