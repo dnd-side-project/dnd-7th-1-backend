cdaa13dc64c753af4099a25a13b8550156e71f5f
ChanoPark <koc081900@korea.ac.kr>
2023-04-26 18:55:24 +0900

    FEAT: 챌린지에 포함된 모든 UC를 조회하기 위한 ItemReader 구현
    1.JpaPagingItemReader의 동작 방식을 참고해, AbstractPagingItemReader를 상속 받아 구현.
    2.챌린지와 관련된 배치 작업에서 활용해야 하므로 빈으로 등록하지 않고, 객체를 생성해서 사용하도록 구현.(싱글톤의 경우 paramMap에서 문제가 발생할 여지가 있음)
    3.chunk와 page의 크기를 10으로 맞춰 한 트랜잭션 내에서 쿼리를 한 번에 처리할 수 있도록 함.

---
+++ src/main/java/com/dnd/ground/global/batch/challenge/ChallengeWithUCItemReader.java
@@ -0,0 +1,138 @@
+package com.dnd.ground.global.batch.challenge;
+
+import com.dnd.ground.domain.challenge.Challenge;
+import com.dnd.ground.domain.challenge.UserChallenge;
+import com.dnd.ground.global.log.CommonLogger;
+import org.springframework.batch.item.database.AbstractPagingItemReader;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.dao.DataAccessResourceFailureException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * @description 챌린지-UC 간 조회를 위한 ItemReader.
+ *              챌린지를 기준으로 chunk를 나누되 해당 챌린지의 UC가 모두 필요한 상황이므로, ItemReader를 상속 받아 커스텀한 구현체.
+ *              챌린지 시작, 종료 등 챌린지 관련 배치 작업에서 사용해야 하므로 싱글톤인 빈으로 관리하지 않고 객체를 생성해서 사용하는 방식으로 활용.
+ *              JpaPagingItemReader.class, AbstractPagingItemReader.class의 동작 방식을 참고해 트랜잭션 관리 및 페이징 적용.
+ *              결과적으로, 챌린지를 기준으로 페이징이 적용된다.
+ * @author  박찬호
+ * @since   2023-04-14
+ * @updated 1. ItemReader 구현 완료
+ *          - 2023-04-26 박찬호
+ */
+public class ChallengeWithUCItemReader extends AbstractPagingItemReader<ChallengeWithUCDto> {
+
+    private EntityManager em;
+    private final EntityManagerFactory emf;
+
+    private int page;
+    private int pageSize;
+    private final Map<String, Object> challengeParamMap;
+    private final CommonLogger logger;
+
+    public ChallengeWithUCItemReader(Map<String, Object> challengeParamMap,
+                                     EntityManagerFactory entityManagerFactory,
+                                     @Qualifier("batchLogger") CommonLogger logger) {
+        this.challengeParamMap = challengeParamMap;
+        this.emf = entityManagerFactory;
+        this.page = getPage();
+        this.pageSize = getPageSize();
+        this.logger = logger;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+        super.setPageSize(pageSize);
+    }
+
+    public int getPage() {
+        return super.getPage();
+    }
+
+    @Override
+    protected void doOpen() throws Exception {
+        super.doOpen();
+
+        em = emf.createEntityManager();
+        if (em == null) {
+            throw new DataAccessResourceFailureException("챌린지 시작 배치: EntityManager를 생성할 수 없습니다.");
+        }
+    }
+
+    /**
+     * AbstractPagingItemReader.class에서 페이지를 읽기 위한 메소드 Override.
+     * 결과 배열 및 트랜잭션 관리를 한다.
+     */
+    @Override
+    protected void doReadPage() {
+        //결과 배열 및 트랜잭션, em 초기화
+        if (results == null) results = new CopyOnWriteArrayList<>();
+        else results.clear();
+
+        em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        em.flush();
+        em.clear();
+
+        try {
+            results.addAll(executeQuery());
+            tx.commit();
+        } catch (Exception e) {
+            logger.errorWrite(String.format("챌린지 시작 배치: 쿼리 중 예외가 발생했습니다. MSG: %s", e.getMessage()));
+            tx.rollback();
+        }
+    }
+
+    /**
+     * 실제 쿼리를 날려서 받는 역할을 한다.
+     */
+    public List<ChallengeWithUCDto> executeQuery() {
+        List<ChallengeWithUCDto> response = new ArrayList<>();
+
+        TypedQuery<Challenge> challengeTypedQuery =
+                em.createQuery(
+                        "SELECT c " +
+                                "FROM Challenge c " +
+                                "WHERE c.created <= :jobParam AND c.status = :status",
+                        Challenge.class);
+
+        if (challengeParamMap != null) {
+            for (Map.Entry<String, Object> param : challengeParamMap.entrySet()) {
+                challengeTypedQuery.setParameter(param.getKey(), param.getValue());
+            }
+        }
+
+
+        List<Challenge> challenges = challengeTypedQuery
+                .setFirstResult(page * pageSize)
+                .setMaxResults(pageSize)
+                .getResultList();
+
+        for (Challenge challenge : challenges) {
+            List<UserChallenge> ucs = em.createQuery(
+                            "SELECT uc " +
+                                    "FROM UserChallenge uc " +
+                                    "WHERE uc.challenge = :challenge", UserChallenge.class)
+                    .setParameter("challenge", challenge)
+                    .getResultList();
+
+            response.add(new ChallengeWithUCDto(challenge, ucs));
+        }
+
+        return response;
+    }
+
+    @Override
+    protected void doJumpToPage(int itemIndex) {
+    }
+}
